219aaa7248bde5a735b79157e1fe9475cbe9672b
gabriel <gmoraes.mat@gmail.com>
2022-04-28 17:17:45 -0300

    Layout dos produtos pronto

--- java.js
+++ java.js
@@ -1,6 +1,17 @@
 let Usuario =0;
 function iniciarPagina(){
-    Usuario = prompt("Qual o seu nome ?")
+   // Usuario = prompt("Qual o seu nome ?")
+
+}
+
+function selection(elemento){
+    const itemSelecionado = document.querySelector('.Linha .selecionado');
+
+    if(itemSelecionado!==null){
+        itemSelecionado.classList.remove('selecionado');
+    }
+
+    elemento.classList.add('selecionado');
 
 }
 
