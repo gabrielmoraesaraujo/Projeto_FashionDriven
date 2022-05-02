c7d6454c73dccc73e00f42602248ad7354a1184e
gabriel <gmoraes.mat@gmail.com>
2022-05-01 22:30:41 -0300

    criação do objeto para envio da API

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
 let Usuario =0;
+let modelo;
+let gola;
+let tecido;
+let objeto = {};
 function iniciarPagina(){
    // Usuario = prompt("Qual o seu nome ?")
 
@@ -8,10 +12,11 @@ function selectionModelo(elemento){
     const itemSelecionado = document.querySelector('.Modelo .selecionado');
 
     if(itemSelecionado!==null){
-        itemSelecionado.classList.remove('selecionado');
+        itemSelecionado.classList.remove('selecionado');      
     }
     elemento.classList.add('selecionado');
-}
+    modelo = elemento.querySelector('.escondido').innerHTML;
+}   
 
 function selectionGola(elemento){
     const itemSelecionado = document.querySelector('.Gola .selecionado');
@@ -20,6 +25,8 @@ function selectionGola(elemento){
         itemSelecionado.classList.remove('selecionado');
     }
     elemento.classList.add('selecionado');
+    gola = elemento.querySelector('.escondido').innerHTML;
+    
 }
 
 function selectionTecido(elemento){
@@ -29,6 +36,66 @@ function selectionTecido(elemento){
         itemSelecionado.classList.remove('selecionado');
     }
     elemento.classList.add('selecionado');
+    tecido = elemento.querySelector('.escondido').innerHTML;
+  
 }
+
+
+//const meuInterval = setInterval(teste,1000);
+function ativarBotao(){
+    verifica();
+
+    
+}
+
+function verifica(){
+    objeto["model"]=0;
+    if(modelo==="t-shirt"){
+        objeto.model=modelo;
+    }
+    if(modelo==="long"){
+        objeto.model=modelo;
+    }
+    if(modelo==="top-tank"){
+        objeto.model=modelo;
+    }    
+
+    objeto["neck"]=0;
+    if(gola==="v-neck"){
+        objeto.neck=gola;
+    }
+    if(gola==="round"){
+        objeto.neck=gola;
+    }
+    if(gola==="polo"){
+        objeto.neck=gola;
+    } 
+
+    objeto["material"]=0;
+    if(tecido==="silk"){
+        objeto.material=tecido;
+    }
+    if(tecido==="cotton"){
+        objeto.material=tecido;
+    }
+    if(tecido==="polyester"){
+        objeto.material=tecido;
+    } 
+
+    //validarURL();
+    //objeto["image"]=0;
+    //if(url!===null){
+    //objeto.image=url;}
+
+
+
+    console.log(objeto);
+
+}
+
+function validarURL(url) {
+    const regra = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+    return regra.test(url);
+  }
 
 iniciarPagina();
